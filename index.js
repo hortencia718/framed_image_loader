774c8def9b4685416ca659b6d61a2531a67fbb03
hortencia cisneros <hortenciaecisneros@gmail.com>
2021-07-07 11:31:41 -0400

    made some corrections in JS file

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // select the attributes for names  
-
+// 
 const urlInput = document.querySelector("input[name='url']");
 const widthInput = document.querySelector("input[name='width']");
 const colorInput = document.querySelector("input[name='color']");
 const image = document.querySelector(".image");
 
-
+// create variable to keep track of the info global
 let url = "";
 let width = 10;
 let color = "#000000";
 
+
+//  hides the image onload 
 function hideImageOnLoad() {
     image.style.display = "none";
 }
 
+// parameters so when called they display 
 function setImage(url, width, color) {
     image.style.display = "block";
     image.style.borderStyle = "solid";
@@ -23,13 +26,13 @@ function setImage(url, width, color) {
      
 }
 
-urlInput.addEventListener("change", ()=>{
-    url= urlInput.nodeValue;
+urlInput.addEventListener("change", () => {
+    url= urlInput.value;
     setImage(url, width, color);
 })
 
-widthInput.addEventListener("change", () =>{
-    width = widthInput, value;
+widthInput.addEventListener("change", () => {
+    width = widthInput.value;
     setImage(url, width, color);
 })
 
